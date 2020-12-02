26d32c996f7e5537ec3b2c36fc93e66601fc4446
Michael <mwellblack@gmail.com>
2020-12-01 19:31:07 -0500

    final model for presentation

--- CS4200_Model.py
+++ CS4200_Model.py
@@ -7,29 +7,38 @@ Created on Tue Nov 10 15:20:13 2020
 """
 
 import tensorflow as tf, pandas as pd, numpy as np, matplotlib.pyplot as plt
-from tensorflow.keras import layers
 from tensorflow.keras.layers import LSTM, Dense, Dropout
-import json, sklearn, time, tensorflow_datasets
-from sklearn.preprocessing import MinMaxScaler,StandardScaler
+import time, shutil
+from sklearn.preprocessing import MinMaxScaler
 from datetime import datetime
-
-#%% Import and prepare data
+import pandas_datareader.data as pdr
 
 #Quick Parameters:
-window_size = 5
+window_size = 10
 test_size = 5
-batchsize = 9
-date = datetime(2020,1,1)
+val_size = 20
+batchsize = 10
+start_date = datetime(2017,1,1)
+end_date = datetime(2020,11,30)
+epoch=20
+log_dir= "/home/mike/Desktop/LSTM"
 
-#Load the data from .csv
-rawdata = pd.read_csv('AMZN.csv')
+#%% Import data and add necessary columns
 
-#Set index as 'Date' and convert to datetime, allows or limiting of date range.
-rawdata=rawdata.set_index('Date')
-rawdata.index=pd.to_datetime(rawdata.index)
-rawdata=rawdata.loc[rawdata.index > date]
+def fetch_data(ticker, start, end):
+    #Fetches the pricing data of ticker from yahoo 
+    try:
+        df=pdr.DataReader(ticker, 'yahoo', start, end)
+        df.to_csv(f'stock_details/{ticker}.csv')
+    except:
+        print("Error")
+    return df
+  
+rawdata=fetch_data('AMZN',start_date, end_date)
 
-#Feature Scaling
+#%% Prepare data for split/training
+
+#Feature scaling, one scaler per column
 column_scalers={}
 data=pd.DataFrame(index=rawdata.index)
 
@@ -38,78 +47,74 @@ for column in rawdata.columns.values:
     data.loc[:,column] = scaler.fit_transform(np.expand_dims(rawdata[column].values, axis=1))
     column_scalers[column] = scaler 
 
-x_samples = []
-y_samples = []
+x_samples, y_samples = [],[]
  
-# Using a 'sliding window' model, partition data into arrays of size 'window_size'
 for i in range(window_size , len(data) , 1):
+    # Using a 'sliding window' model, partition data into arrays of size 'window_size'
     x_sample = np.array(data[i-window_size:i])
     y_sample = np.array(data[i:i+1]['Adj Close'])
     x_samples.append(x_sample)
     y_samples.append(y_sample)
  
-# Reshape the Input as a 3D (number of samples, Time Steps, Features)
-x_data = np.array(x_samples)                                  
-x_data = x_data.reshape(x_data.shape[0],x_data.shape[1], x_data.shape[2])
+# Convert lists to arrays
+x_data, y_data = np.array(x_samples), np.array(y_samples)
 
-y_data=np.array(y_samples)
-y_data=y_data.reshape(y_data.shape[0], y_data.shape[1])
+tomorrow = np.array(data[len(data)-window_size:])                               
+tomorrow = tomorrow.reshape(1,len(tomorrow),len(tomorrow[0]))
+#%% Split data into training, testing, and validation sets
 
-#%% Split data into training and testing
-
-train_size = x_data.shape[0] - 2*test_size
+train_size = x_data.shape[0] - test_size - val_size
 
 x_train = x_data[0:train_size]
 y_train = y_data[0:train_size]
 
-x_val = x_data[train_size:train_size+test_size]
-y_val = y_data[train_size:train_size+test_size]
+x_val = x_data[train_size:train_size+val_size]
+y_val = y_data[train_size:train_size+val_size]
 
-train_size += test_size
+train_size += val_size
 
 x_test = x_data[train_size:train_size+test_size]
 y_test = y_data[train_size:train_size+test_size]
 
 #%% Build the Model
 
-# Defining Input shapes for LSTM
-TimeSteps=window_size
-TotalFeatures=x_data.shape[2]
+#Define input shape for LSTM
+features = x_data.shape[2]
 
 #Initialize the model
 model = tf.keras.Sequential()
+model.add(LSTM(units = 200,activation='relu', input_shape = (window_size, features)))
+model.add(Dense(units=200))
+model.add(Dense(units=100))
+model.add(Dense(units=1))
 
-model.add(LSTM(units = 64, activation='relu', input_shape = (TimeSteps, TotalFeatures), return_sequences=True))
-model.add(Dropout(0.2))
-model.add(LSTM(units = 64, activation='relu', return_sequences=True))
-model.add(Dropout(0.2))
-model.add(LSTM(units = 64, activation='relu'))
-model.add(Dense(units = 1))
-model.compile(optimizer = 'adam', loss = 'huber_loss', metrics='mean_absolute_error')
+model.compile(optimizer = 'adam', loss = 'mse', metrics='mean_absolute_error')
   
 #%% Train the model
 
 #Create tensorboard log files
-logdir="/home/mike/Desktop/LSTM" + datetime.now().strftime("%Y%m%d")
+logdir=log_dir + datetime.now().strftime("%Y%m%d")
+try: shutil.rmtree(logdir)
+except: pass
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 
 # Measuring the time taken by the model to train
-StartTime=time.time()
+start=time.time()
  
-# Fitting the RNN to the Training set using GPU0
+#Fitting the model to the Training set using GPU0
 with tf.device('/GPU:0'):
     history = model.fit(x_train,
                         y_train,
                         batch_size=batchsize,
-                        epochs=20,
+                        epochs=epoch,
                         validation_data=(x_val,y_val),
                         callbacks=[tensorboard_callback])
  
-EndTime=time.time()
-print("## Total Time Taken: ", round((EndTime-StartTime)/60), 'Minutes ##')
-
+end=time.time()
+print('**** Training Duration: ', round((end-start)/60), 'Minutes ****')
 
 #%% Assess Accuracy
+
 # Making predictions on test data
 pred_index = np.where(data.columns=='Adj Close')[0][0]
 
@@ -120,16 +125,12 @@ predicted_Price = column_scalers['Adj Close'].inverse_transform(predicted_Price)
 orig=y_test
 orig=column_scalers['Adj Close'].inverse_transform(y_test)
  
-# Accuracy of the predictions
-print('Accuracy:', 100 - (100*(abs(orig-predicted_Price)/orig)).mean())
-
-
 plt.plot(predicted_Price, color = 'blue', label = 'Predicted Price')
 plt.plot(orig, color = 'lightblue', label = 'Original Price')
  
 plt.title('Stock Price Predictions')
 plt.xlabel('Trading Date')
-plt.xticks(range(x_test.shape[0]), data.tail(x_test.shape[0]).index)
+plt.xticks(range(x_test.shape[0]), data[len(data)-x_test.shape[0]:len(data)].index)
 plt.ylabel('Stock Price')
  
 plt.legend()
@@ -138,18 +139,18 @@ fig.set_figwidth(20)
 fig.set_figheight(6)
 plt.show()
 
-#%% 
+#%% Show model predictions on the full dataset
+
 # Generating predictions on full data
-TrainPredictions=column_scalers['Adj Close'].inverse_transform(model.predict(x_train))
-TestPredictions=column_scalers['Adj Close'].inverse_transform(model.predict(x_test))
+x_predictions=column_scalers['Adj Close'].inverse_transform(model.predict(x_data))
+#TestPredictions=column_scalers['Adj Close'].inverse_transform(model.predict(x_test))
  
-FullDataPredictions=np.append(TrainPredictions, TestPredictions)
-FullDataOrig=column_scalers['Adj Close'].inverse_transform(y_samples)
+#FullDataPredictions=np.append(TrainPredictions, TestPredictions)
+y_actual=column_scalers['Adj Close'].inverse_transform(y_data)
  
 # plotting the full data
-plt.plot(FullDataPredictions, color = 'blue', label = 'Predicted Price')
-plt.plot(FullDataOrig , color = 'lightblue', label = 'Original Price')
- 
+plt.plot(x_predictions, color = 'blue', label = 'Predicted Price')
+plt.plot(y_actual , color = 'lightblue', label = 'Original Price')
  
 plt.title('Stock Price Predictions')
 plt.xlabel('Trading Date')
@@ -159,4 +160,29 @@ fig=plt.gcf()
 fig.set_figwidth(20)
 fig.set_figheight(8)
 plt.show()
+
+#%% Stats
+
+#Prepare gradient dataframe from historic data
+df1=pd.DataFrame(rawdata['Adj Close'][window_size:])
+df1.index=rawdata.index[window_size:]
+df1['Gradient']=df1['Adj Close']-df1['Adj Close'].shift(1)
+df1=df1['Gradient'].transform(lambda x: 1 if x > 0 else 0)
+
+#Prepare gradient dataframe from predictions
+temp=column_scalers['Adj Close'].inverse_transform(model.predict(x_data))
+test_pred=pd.DataFrame(temp)
+test_pred.index=rawdata.index.values[window_size:]
+test_pred['Gradient']=test_pred[0]-test_pred[0].shift(1)
+test_pred=test_pred['Gradient'].transform(lambda x: 1 if x > 0 else 0)
+
+grad_acc = sum(df1 == test_pred)/len(df1)
+grad_acc_test = sum(df1[len(test_pred)-test_size:] == test_pred[len(test_pred)-test_size:])/test_size
+tomorrow_price = column_scalers['Adj Close'].inverse_transform(model.predict(tomorrow))[0][0]
+
+print('Gradient Accuracy (all data): ', round(grad_acc,3))
+print('Gradient Accuracy (test data): ', round(grad_acc_test,3))
+print('Tomorrows Price: ', round(tomorrow_price,2))
+
+print(rawdata['Adj Close'].head(10), x_predictions[0:10])
 
